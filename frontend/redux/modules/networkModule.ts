045324e9492dad21b47317ea79e6ec1ff9a46de0
Austin <austin444@unseen.is>
2020-10-31 14:29:45 -0400

    updated getall method

--- frontend/redux/modules/networkModule.ts
+++ frontend/redux/modules/networkModule.ts
@@ -1,33 +1,30 @@
+import {URL} from "../config";
+
 const NetworkActions = {
   GET_ALL_NETWORKS: "networks/GET_ALL_NETWORKS",
 };
 
 export function getAllNetworks(network) {
-  return (dispatch, getState) => {
-    // return firebase
-    //   .firestore()
-    //   .collection("Exercises")
-    //   .where("user", "==", user)
-    //   .get()
-    //   .then((result) => {
-    //     // result.docs.forEach(doc => {
+  return async (dispatch, getState) => {
+    //let bodyStr = JSON.stringify({query: "{query goes here}"});
+    let myHeaders = new Headers();
+    let networks = [];
+    myHeaders.append("Content-Type", "application/json");
+   try{
+     
+      let response = await fetch(URL + "/network", {
+        method: "GET",
+        headers: myHeaders
+      });
+      let json = await response.json();
 
-    //     // })
-    // try{}
-    //     const networks = [];
-    //     result.forEach(function (doc) {
-    //       workouts.push({ ...doc.data(), id: doc.id });
-    //     });
-    //     dispatch({ type: ExerciseActions.GET_EXERCISES, payload: workouts });
-    //     return workouts;
-    //   //})
-    //   .catch((error) => {
-    //     const payload = { hasError: true, message: error };
-    //     dispatch({ type: ExerciseActions.EXERCISES_ERROR, payload: payload });
-    //     return payload;
-    //   //});
+       networks = [json];
+   }catch(error){
+     console.log(error);
 
-    const networks = ["test"];
+   }
+
+    
 
     dispatch({ payload: networks });
   };
