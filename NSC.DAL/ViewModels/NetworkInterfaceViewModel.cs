9e97c598b61ded9b89f2af8e8198329cf480a0fa
s-balandin <53315482+s-balandin@users.noreply.github.com>
2020-10-31 15:52:02 -0400

    Added more viewmodels

---
+++ NSC.DAL/ViewModels/NetworkInterfaceViewModel.cs
@@ -0,0 +1,26 @@
+﻿using NSC.DAL.Database;
+
+namespace NSC.DAL.ViewModels
+{
+    public class NetworkInterfaceViewModel
+    {
+        public NetworkInterfaceViewModel() { }
+
+        public NetworkInterfaceViewModel(NetworkInterface networkInterface)
+        {
+
+        }
+
+        public int DeviceId { get; set; }
+        public string InternalIP { get; set; }
+        public string MACAddress { get; set; }
+        public string InterfaceId { get; set; }
+        public string InterfaceName { get; set; }
+        public string InterfaceDescription { get; set; }
+        public string InterfaceType { get; set; }
+        public int? InterfaceSpeed { get; set; }
+        public string InterfaceStatus { get; set; }
+        public DeviceViewModel Device { get; set; }
+        public ICollection<NetworkStatTestViewModel> NetworkStatTests { get; set; }
+    }
+}
---
+++ NSC.DAL/ViewModels/NetworkStatTestViewModel.cs
@@ -0,0 +1,52 @@
+﻿using NSC.DAL.Database;
+using System;
+
+namespace NSC.DAL.ViewModels
+{
+    public class NetworkStatTestViewModel
+    {
+        public NetworkStatTestViewModel() { }
+        public NetworkStatTestViewModel(NetworkStatTest networkStatTest)
+        {
+            DeviceId = networkStatTest.DeviceId;
+            TestRunTime = networkStatTest.TestRunTime;
+            TestStatus = networkStatTest.TestStatus;
+            Jitter = networkStatTest.Jitter;
+            Latency = networkStatTest.Latency;
+            DownloadBandwidth = networkStatTest.DownloadBandwidth;
+            DownloadSpeed = networkStatTest.DownloadSpeed;
+            DownloadElapsed = networkStatTest.DownloadElapsed;
+            UploadBandwidth = networkStatTest.UploadBandwidth;
+            UploadSpeed = networkStatTest.UploadSpeed;
+            UploadElapsed = networkStatTest.UploadElapsed;
+            PacketLoss = networkStatTest.PacketLoss;
+            ISP = networkStatTest.ISP;
+            ActiveURL = networkStatTest.ActiveVPN;
+            ResultId = networkStatTest.ResultId;
+            ResultURL = networkStatTest.ResultURL;
+            Device = new DeviceViewModel(networkStatTest.Device);
+            NetworkInterface = new NetworkInterfaceViewModel(networkStatTest.NetworkInterface);
+            SpeedTestServer = new SpeedTestServerViewModel(networkStatTest.SpeedTestServer);
+        }
+
+        public int DeviceId { get; set; }
+        public DateTime TestRunTime { get; set; }
+        public string TestStatus { get; set; }
+        public decimal? Jitter { get; set; }
+        public decimal? Latency { get; set; }
+        public int? DownloadBandwidth { get; set; }
+        public int? DownloadSpeed { get; set; }
+        public int? DownloadElapsed { get; set; }
+        public int? UploadBandwidth { get; set; }
+        public int? UploadSpeed { get; set; }
+        public int? UploadElapsed { get; set; }
+        public decimal? PacketLoss { get; set; }
+        public string ISP { get; set; }
+        public bool? ActiveURL { get; set; }
+        public string ResultId { get; set; }
+        public string ResultURL { get; set; }
+        public DeviceViewModel Device { get; set; }
+        public NetworkInterfaceViewModel NetworkInterface { get; set; }
+        public SpeedTestServer SpeedTestServer { get; set; }
+    }
+}
