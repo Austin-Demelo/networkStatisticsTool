599867c227edb6466f5978bf73f97463ce7fe82a
Austin <austin444@unseen.is>
2020-11-07 17:24:07 -0500

    Merge branch 'main' of https://github.com/ChrisAKlomp/scchaumo into main
    # Conflicts:
    #	NSCController/App_Start/WebApiConfig.cs
    #	NSCController/Web.config

--- NSC.Service/Services/UserService.cs
+++ NSC.Service/Services/UserService.cs
@@ -1,87 +1,92 @@
-﻿using System;
+﻿using NSC.DAL.Database;
+using NSC.DAL.Models;
+using NSC.DAL.Repository;
+using NSC.DAL.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NSC.Service.Services
 {
-    //public class UserService
-    //{
-    //    private UserModel _networkModel;
+    public class UserService
+    {
+        private UserModel _networkModel;
 
-    //    public UserService()
-    //    {
-    //        _networkModel = new UserModel();
-    //    }
-    //    public int Update(UserViewModel vm)
-    //    {
-    //        UpdateStatus opStatus = UpdateStatus.Failed;
-    //        try
-    //        {
-    //            User net = new User();
-    //            net.UserName = vm.UserName;
-    //            opStatus = _networkModel.Update(net);
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            //Compiler figures out the method name using the System.Reflection library
-    //            Console.WriteLine("Problem in " + GetType().Name + " " + MethodBase.GetCurrentMethod().Name + " " + ex.Message);
+        public UserService()
+        {
+            _networkModel = new UserModel();
+        }
+        public int Update(UserViewModel vm)
+        {
+            UpdateStatus opStatus = UpdateStatus.Failed;
+            try
+            {
+                User net = new User();
+                net.UserName = vm.UserName;
+                opStatus = _networkModel.Update(net);
+            }
+            catch (Exception ex)
+            {
+                //Compiler figures out the method name using the System.Reflection library
+                Console.WriteLine("Problem in " + GetType().Name + " " + MethodBase.GetCurrentMethod().Name + " " + ex.Message);
 
-    //        }
-    //        return Convert.ToInt16(opStatus);
-    //    }
-    //    public int Add(UserViewModel vm)
-    //    {
+            }
+            return Convert.ToInt16(opStatus);
+        }
+        public int Add(UserViewModel vm)
+        {
 
-    //        try
-    //        {
-    //            User net = new User();
-    //            net.UserName = vm.UserName;
-    //            return _networkModel.Add(net);
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            Console.WriteLine("Problem in " + GetType().Name + " " + MethodBase.GetCurrentMethod().Name + " " + ex.Message);
-    //            throw ex;
-    //        }
-    //    }
+            try
+            {
+                User net = new User();
+                net.UserName = vm.UserName;
+                return _networkModel.Add(net);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Problem in " + GetType().Name + " " + MethodBase.GetCurrentMethod().Name + " " + ex.Message);
+                throw ex;
+            }
+        }
 
-    //    public int Delete(int Id)
-    //    {
-    //        try
-    //        {
-    //            return _networkModel.Delete(Id);
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            Console.WriteLine("Problem in " + GetType().Name + " " + MethodBase.GetCurrentMethod().Name + " " + ex.Message);
-    //            throw ex;
-    //        }
-    //    }
-    //    public List<UserViewModel> GetAll()
-    //    {
-    //        List<UserViewModel> networkViewModels = new List<UserViewModel>();
-    //        try
-    //        {
-    //            foreach (User network in _networkModel.GetAll())
-    //            {
+        public int Delete(int Id)
+        {
+            try
+            {
+                return _networkModel.Delete(Id);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Problem in " + GetType().Name + " " + MethodBase.GetCurrentMethod().Name + " " + ex.Message);
+                throw ex;
+            }
+        }
+        public List<UserViewModel> GetAll()
+        {
+            List<UserViewModel> networkViewModels = new List<UserViewModel>();
+            try
+            {
+                foreach (User network in _networkModel.GetAll())
+                {
 
-    //                List<DeviceViewModel> deviceViewModels = new List<DeviceViewModel>();
-    //                foreach (Device device in network.Devices)
-    //                {
-    //                    deviceViewModels.Add(new DeviceViewModel(device));
-    //                }
-    //                networkViewModels.Add(new UserViewModel(network));
-    //            }
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            Console.WriteLine("Problem in " + GetType().Name + " " + MethodBase.GetCurrentMethod().Name + " " + ex.Message);
-    //            throw ex;
-    //        }
+                    List<DeviceViewModel> deviceViewModels = new List<DeviceViewModel>();
+                    foreach (Device device in network.Devices)
+                    {
+                        deviceViewModels.Add(new DeviceViewModel(device));
+                    }
+                    networkViewModels.Add(new UserViewModel(network));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Problem in " + GetType().Name + " " + MethodBase.GetCurrentMethod().Name + " " + ex.Message);
+                throw ex;
+            }
 
-    //        return networkViewModels;
-    //    }
-    //}
+            return networkViewModels;
+        }
+    }
 }
---
+++ frontend/src/components/UserForm.jsx
@@ -0,0 +1,126 @@
+import { Button, Card, CardContent, TextField } from "@material-ui/core/";
+import { createUser, getAllUsers, updateUser } from '../redux/modules/userModule'
+
+import React from 'react'
+import { connect } from 'react-redux'
+
+class UserForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            formData: {
+                UserName: '',
+            },
+            //Through validateFields(), will populate field value (Ex UserName) from formData, with error message string
+            //EX fromValidation: { UserName: 'This field is required' }
+            //Used in Field Component's properties *error* and *helperText*
+            //EX error={!!this.state.formValidation.UserName} - BOOLEAN
+            //EX helperText={this.state.formValidation.UserName || ''} - STRING
+            formValidation: { } 
+        }
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+        this.componentDidMount = this.componentDidMount.bind(this);
+    }
+
+    componentDidMount() {
+        if(this.props.editUser) {
+            this.setState({
+                //Load form data, from parent property *editUser* from UserList
+                formData: {
+                    UserName: this.props.editUser.UserName
+                }
+            });
+        }
+    }
+    validateFields() {
+        let updatedValidation = {}; //Initialize empty validation object
+        Object.keys(this.state.formData)
+        .forEach((field) => {
+            const value = this.state.formData[field];
+            switch(field) {
+                case 'UserName':
+                  if(!value) {
+                    updatedValidation[field] = "This field is required"
+                  }
+                  break;
+                default:
+                  // code block
+              }
+            
+        });
+        this.setState({
+            formValidation: updatedValidation
+        });
+    }
+
+    async onFormSubmit(e) {
+        let user = {...this.state.formData};
+        await this.validateFields();
+        if(!Object.keys(this.state.formValidation).length) {
+            if(this.props.editUser) {
+                //Update the User
+                user.Id = this.props.editUser.Id;
+                user.UserName = this.props.editUser.UserName;
+                user.RoleId = this.props.editUser.RoleId;
+                this.props.updateUser(user)
+                .then((user) => {
+                    this.props.handleClose(); //Passed as argument from UserList
+                });
+            }
+            else {
+                //Add the User
+                this.props.createUser(user)
+                .then((user) => {
+                    this.props.handleClose(); //Passed as argument from UserList
+                });
+            }
+        }
+       
+    }
+
+
+    render() {
+        return (
+            <Card>
+                <CardContent>
+                    <TextField
+                        onChange={(e) => this.setState({formData: {...this.state.formData, UserName: e.target.value}})}
+                        placeholder="User Name"
+                        autoFocus={true} //Needed on the first field of each form
+                        value={this.state.formData.UserName  || ''}
+                        error={!!this.state.formValidation.UserName}
+                        helperText={this.state.formValidation.UserName || ''}
+                        label="User Name"
+                        style = {{display: 'block', width: 300}}
+                    />
+                    <Button color="primary" onClick={this.onFormSubmit}>
+                        {(this.props.editUser === undefined ? 'Add' : 'Update')} User
+                    </Button>
+                </CardContent>
+            </Card>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        userList: state.users.users,
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        getAllUsers: () => dispatch(getAllUsers()),
+        createUser: (user) => dispatch(createUser(user)),
+        updateUser: (user) => dispatch(updateUser(user)),
+    }
+}
+// Must have {forwardRef: true} if passing args from parent
+export default connect(
+        mapStateToProps, 
+        mapDispatchToProps, 
+        null, 
+        {forwardRef: true}
+    )(UserForm)
+
+
